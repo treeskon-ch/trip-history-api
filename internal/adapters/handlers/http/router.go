234d2b7cf13a79bffdd7a92f2cd82025922bd307
Treesukon Chinnapad <treesukonchinnapad@gmail.com>
2026-09-06 13:34:55 +0700

    fix: explicit CORS origins for vercel

--- internal/adapters/handlers/http/router.go
+++ internal/adapters/handlers/http/router.go
@@ -18,7 +18,11 @@ func SetupRouter(
 
 	// CORS config
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"*"},
+		AllowOrigins: []string{
+			"https://trip-history-web.vercel.app",
+			"http://localhost:3000",
+			"http://localhost:3001",
+		},
 		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
